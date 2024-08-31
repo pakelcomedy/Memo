ced2e65b59d9bc0a2a8711fa0f23985472a1e94d
Cursed Pakel <danieldna1411@gmail.com>
2024-08-31 21:40:20 +0700

    GRADLE FIX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,40 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
-    kotlin("parcelize") // Correct plugin ID
+    id("org.jetbrains.kotlin.android") version "1.9.0"
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.mymemoapp"
+        applicationId = "com.pakelcomedy.memo"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    // Add the namespace property
+    namespace = "com.pakelcomedy.memo"
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
