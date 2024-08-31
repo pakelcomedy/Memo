bcffcfa6cd32cd2cec57045da384ea7c3475484d
Cursed Pakel <danieldna1411@gmail.com>
2024-08-31 08:50:33 +0700

    PUSH MODEL

---
+++ app/src/main/java/com/pakelcomedy/memo/repository/NoteRepository.kt
@@ -0,0 +1,19 @@
+package com.pakelcomedy.memo.repository
+
+import androidx.room.Query
+import com.eneskaen.notesapp.database.NoteDatabase
+import com.eneskaen.notesapp.model.Note
+
+class NoteRepository(private  val db : NoteDatabase) {
+
+    suspend fun insertNote(note : Note) = db.getNoteDao().insertNote(note)
+
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+
+}
