76437257d9b1b05a61c990cd9f48f10755441f0d
Cursed Pakel <danieldna1411@gmail.com>
2024-08-31 08:48:03 +0700

    PUSH VIEW MODEL AJA

---
+++ app/src/main/java/com/pakelcomedy/memo/viewmodel/NoteViewModel.kt
@@ -0,0 +1,33 @@
+package com.eneskaen.notesapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Query
+import com.eneskaen.notesapp.model.Note
+import com.eneskaen.notesapp.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(
+    app: Application,
+    private val noteRepository: NoteRepository):
+    AndroidViewModel(app) {
+
+    fun addNote(note : Note) = viewModelScope.launch {
+        noteRepository.insertNote(note)
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch {
+        noteRepository.deleteNote(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch {
+        noteRepository.updateNote(note)
+    }
+
+    fun getAllNotes() = noteRepository.getAllNotes()
+
+    fun searchNote(query: String?) = noteRepository.searchNote(query)
+
+}
---
+++ app/src/main/java/com/pakelcomedy/memo/viewmodel/NoteViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.eneskaen.notesapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.eneskaen.notesapp.repository.NoteRepository
+
+class NoteViewModelFactory(
+    val app: Application,
+    private val noteRepository: NoteRepository
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NoteViewModel(app, noteRepository) as T
+    }
+}
